d465de7477ec5c5da711b75b6f0d9688fa8f2d68
Brian Onang'o <surgbc@gmail.com>
2022-10-24 16:25:39 +0300

    fixed ACL

--- server/listeners/auth/defaults.go
+++ server/listeners/auth/defaults.go
@@ -1,25 +1,30 @@
 package auth
 
+import "fmt"
+
 // Allow is an auth controller which allows access to all connections and topics.
 type Allow struct{}
 
 // Authenticate returns true if a username and password are acceptable. Allow always
 // returns true.
-func (a *Allow) Authenticate(user, password []byte) bool {
-	return true
-}
-
-// ACL returns no error if a user has access permissions to read or write on a topic.
-// Allow always returns nil, nil.
 func (a *Allow) Authenticate(user, password []byte) (interface{}, error) {
 	return nil, nil
+}
+
+// ACL returns true if a user has access permissions to read or write on a topic.
+// Allow always returns true.
+func (a *Allow) ACL(user []byte, topic string, write bool) bool {
+	return true
 }
 
 // Disallow is an auth controller which disallows access to all connections and topics.
 type Disallow struct{}
 
-// Authenticate returns nil error if a username and password are acceptable. Disallow always
-// returns error.
+// Authenticate returns true if a username and password are acceptable. Disallow always
+// returns false.
+// func (d *Disallow) Authenticate(user, password []byte) bool {
+// 	return false
+// }
 func (d *Disallow) Authenticate(user, password []byte) (interface{}, error) {
 	return nil, fmt.Errorf("")
 }
